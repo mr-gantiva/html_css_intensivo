df21f8d14f6b539e5f10e4cc165a87f7a4f442dc
mr-gantiva <ivaganva@gmail.com>
2023-07-06 19:03:27 -0400

    add animation and create landing_pages.js

---
+++ assets/js/landing_page.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.logo', { delay: 100});
+ScrollReveal().reveal('.navigation', { delay: 150});
+ScrollReveal().reveal('.content', { delay: 500});
+ScrollReveal().reveal('.sci', { delay: 600});
